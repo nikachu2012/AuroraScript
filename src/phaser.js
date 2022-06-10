1a2eae96340b0ae74e34b9a4f6c810385d76b760
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-06-11 00:15:18 +0900

    [add] AuroraScriptをminifyし、解析パーサへ流すものを追加

--- src/phaser.js
+++ src/phaser.js
@@ -1,1 +1,13 @@
-        
+const codeSplit = (code) => {
+  //コマンドに変形
+  code = code.replace(/\r\n/g, "").replace(/\t/g, "");
+  code = code.split(/(?<!\\);/g);
+  console.log(code);
+
+  for (let index = 0; index < code.length; index++) {
+    const element = code[index];
+
+    phaser(element);
+  }
+};
+
